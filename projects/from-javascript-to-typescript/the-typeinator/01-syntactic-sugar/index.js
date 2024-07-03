ca056868fa4307dcd9eaa659fa071ad3bc582145
TraTran23082004 <135215395+TraTran23082004@users.noreply.github.com>
2024-07-03 23:50:03 +1000

    finish task

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -1,4 +1,34 @@
 // Put your announceMachines function here! ✨
 // See ./original.js for its older JavaScript code.
 
+/*
+
+In `index.js`, export an `announceMachines` function that:
+
+- Uses `let` and `const` instead of `var` for variables
+- Uses `...` spread for arguments instead of `arguments`
+- Uses a `for`-`of` loop instead of a loop
+- Uses <code>\`</code> template string literal concatenation instead of `+`
+
+*/
+
+function announceMachines(announce, ...machines) {
+	let labelsCount = 0;
+
+	for (const machine of machines) {
+		let label;
+
+		if (machine.label) {
+			label = machine.label;
+			labelsCount += 1;
+		} else {
+			label = `Make: ${machine.make}; Model: ${machine.model}`;
+		}
+
+		announce(label);
+	}
+
+	return labelsCount;
+}
+
 module.exports.announceMachines = announceMachines;
--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,46 @@
 // Put your checkEmotion and speak functions here! ✨
 // See ./original.js for their older JavaScript code.
 
+/*
+function checkEmotion(knownEmotions, emotion, callback) {
+	// Simulate database processing time by waiting a second...
+	setTimeout(() => {
+		callback(knownEmotions.has(emotion));
+	}, 1000);
+}
+*/
+
+async function checkEmotion(knownEmotions, emotion) {
+	// Simulate database processing time by waiting a second...
+	await new Promise((resolve) => {
+		setTimeout(resolve, 1000);
+	});
+
+	return knownEmotions.has(emotion);
+}
+
+/*
+function speak(knownEmotions, newEmotion, phrase, callback) {
+	checkEmotion(knownEmotions, newEmotion, (hasEmotion) => {
+		if (hasEmotion) {
+			callback(`"${phrase}" (${newEmotion})`);
+		} else {
+			callback(
+				undefined,
+				new Error(`Does not compute. I do not understand ${newEmotion}.`)
+			);
+		}
+	});
+} 
+*/
+
+async function speak(knownEmotions, newEmotion, phrase) {
+	if (!(await checkEmotion(knownEmotions, newEmotion))) {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
+	}
+
+	return `"${phrase}" (${newEmotion})`;
+}
+
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
